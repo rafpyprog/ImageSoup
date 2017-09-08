922e8c9a6d7809dbf7686a0d00299f6e9fe929b6
rafael <rafael.alves.ribeiro@gmail.com>
2017-09-08 00:14:46 -0300

    Correct sleplling on colors.py

--- colors.py
+++ colors.py
@@ -10,7 +10,6 @@ def color_analysis(image):
         image = image.convert('RGB')
 
     # initialize a Counter starting from zero
-    CSS3_COLORS = webcolors.css3_names_to_hex.keys()
     color_counter = Counter({color:0 for color in Counter(CSS3_COLORS)})
     for pixel_count, RGB in image.getcolors(image.width * image.height):
         color_name = get_colour_name(RGB)
@@ -23,19 +22,20 @@ def color_analysis(image):
     return color_counter
 
 
-def closest_colour(requested_colour):
-    min_colours = {}
+def closest_color(requested_color):
+    min_colors = {}
     for key, name in webcolors.css3_hex_to_names.items():
         r_c, g_c, b_c = webcolors.hex_to_rgb(key)
-        rd = (r_c - requested_colour[0]) ** 2
-        gd = (g_c - requested_colour[1]) ** 2
-        bd = (b_c - requested_colour[2]) ** 2
-        min_colours[(rd + gd + bd)] = name
-    return min_colours[min(min_colours.keys())]
+        rd = (r_c - requested_color[0]) ** 2
+        gd = (g_c - requested_color[1]) ** 2
+        bd = (b_c - requested_color[2]) ** 2
+        min_colors[(rd + gd + bd)] = name
+    return min_colors[min(min_colors.keys())]
 
-def get_colour_name(requested_colour):
+
+def get_color_name(requested_color):
     try:
-        colour_name = actual_name = webcolors.rgb_to_name(requested_colour)
+        color_name = actual_name = webcolors.rgb_to_name(requested_color)
     except ValueError:
-        colour_name = closest_colour(requested_colour)
-    return colour_name
+        color_name = closest_color(requested_color)
+    return color_name
