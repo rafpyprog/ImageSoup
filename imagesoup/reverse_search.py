ae41968e1e05caf1fadb9ebc2aae467aa5f85a86
rafpyprog <rafael.alves.ribeiro@gmail.com>
2017-09-12 18:50:39 -0300

    Reverse search

---
+++ imagesoup/reverse_search.py
@@ -0,0 +1,10 @@
+import requests
+
+
+filePath = 'D:\\Projetos\\#DEV Python\\ImageSoup\\imagesoup\\agua.png'
+searchUrl = 'http://www.google.hr/searchbyimage/upload'
+multipart = {'encoded_image': (filePath, open(filePath, 'rb')), 'image_content': ''}
+response = requests.post(searchUrl, files=multipart, allow_redirects=False)
+fetchUrl = response.headers['Location']
+
+webbrowser.open(fetchUrl)
